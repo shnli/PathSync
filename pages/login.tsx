f6c5e437be7a68e99ddcb0611e6458f348d81ace
Li Shen <lililogan@gmail.com>
2023-07-27 15:44:05 -0700

    landing

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useRouter } from "next/router";
 import React, { useState } from 'react';
 import Image from 'next/image';
-
+import Link from 'next/link'
 
 
 
@@ -98,22 +98,28 @@ export default function Login() {
 
   return (
     <div className='flex flex-col items-center justify-center h-screen md:bg-gray-100 '>
+        <Link href="/welcome" className='absolute top-4 md:top-8 lg:top-10 left-4 md:left-12 flex-col justify-center items-center  hover:underline'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                </svg>
+        </Link>
         
         <div className='md:rounded-xl md:shadow-md md:bg-white max-w-[600px]'>
             <div className='flex items-center justify-center text-center pt-12'>
                 <Image
-                    src="/stats.svg"
+                    src="/login.svg"
                     alt="image"
                     width={200}
                     height={100}
                     className="rounded-lg"
                 />
-                <div className='flex justify-center font-bold text-xl text-blue-400'>PathSync</div>
+                <div className='flex justify-center font-bold text-xl text-primary-blue'>PathSync</div>
             </div>
 
-        <div className="mx-auto w-full px-12 py-16">
-            <div className='flex justify-center text-3xl font-bold text-gray-500 pb-8'>Welcome!</div>
-            <div className='flex justify-center text-sm text-gray-400 pb-8 px-12 text-center'>Transform the way you manage and analyze collaborative processes today.</div>
+        <div className="mx-auto w-full px-12 pt-8 pb-12">
+            <div className='flex justify-center text-3xl font-bold text-gray-500 pb-4'>Welcome!</div>
+            <div className='flex justify-center text-sm text-primary-blue pb-4 px-12 text-center font-bold '>Transform the way you manage and analyze collaborative processes today.</div>
+            <div className='flex justify-center text-sm text-gray-400 pb-4 px-12 text-center text-xs'>To use PathSync, we ask you to create an account to save your work. Worry not, there are no strings attached and you will not receive any emails or notifications.</div>
 
             <div className="mx-auto max-w-md">
             <div className="pt-4">
@@ -146,8 +152,8 @@ export default function Login() {
             </div>
 
             <div className='flex justify-center pt-12 gap-4'>
-            <button onClick={getUser} className='px-16 py-2 border-2 border-blue-400 rounded-md text-blue-400 hover:shadow-none shadow-md'>Log in</button>
-            <button onClick={newUser} className='px-16 py-2 rounded-md bg-blue-400 text-white hover:shadow-none shadow-md'>Sign Up</button>
+            <button onClick={getUser} className='px-16 py-2 border-2 border-primary-blue rounded-md text-primary-blue hover:shadow-none shadow-md'>Log in</button>
+            <button onClick={newUser} className='px-16 py-2 rounded-md bg-primary-blue text-white hover:shadow-none shadow-md'>Sign Up</button>
             </div>
 
             {error && <div className="text-red-500 text-center mt-4">{error}</div>}
